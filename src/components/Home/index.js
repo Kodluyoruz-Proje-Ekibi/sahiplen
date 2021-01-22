d3fce4912be979a14f14d8113f4db1ef04c25b0a
oguzduran <oguzduran@outlook.com.tr>
2021-01-22 18:14:52 +0300

    components added

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeGallery from '../HomeGallery';
+import Blog from '../Blog';
+export default function Home() {
+	return (
+		<div>
+			<HomeGallery />
+			<Blog />
+		</div>
+	);
+}
