53565dfda6b1d25aadfcba720582caf45d9ca7b2
busrablt <busrablt2121@gmail.com>
2021-02-01 21:08:47 +0300

    created login component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeGallery from '../HomeGallery';
-import Blog from '../Blog';
+import Blog from '../Blog'; 
+import Login from '../Login/Login';
 export default function Home() {
 	return (
 		<div>
 			<HomeGallery />
 			<Blog />
+			<Login/>
 		</div>
 	);
 }
