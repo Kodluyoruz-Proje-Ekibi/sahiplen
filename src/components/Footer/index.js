a10ccc3628da47cc89fe03a80080f828c570b267
oguuzduran <oguuzduran@outlook.com.tr>
2021-02-02 07:39:33 +0300

    a href bug fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,7 +23,7 @@ function Footer() {
 					<div className="row">
 						<div className="col-lg-3 col-md-6 footer-contact">
 							<h3>
-								BizLand<span>.</span>
+								Sahiplen<span>.</span>
 							</h3>
 							<p>
 								A108 Adam Street <br />
@@ -42,19 +42,19 @@ function Footer() {
 							<h4>Useful Links</h4>
 							<ul>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Home</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Home</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">About us</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>About us</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Services</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Services</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Terms of service</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Terms of service</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Privacy policy</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Privacy policy</a>
 								</li>
 							</ul>
 						</div>
@@ -63,19 +63,19 @@ function Footer() {
 							<h4>Our Services</h4>
 							<ul>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Web Design</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Web Design</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Web Development</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Web Development</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Product Management</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Product Management</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Marketing</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Marketing</a>
 								</li>
 								<li>
-									<i className="bx bx-chevron-right"></i> <a href="#">Graphic Design</a>
+									<i className="bx bx-chevron-right"></i> <a href={() => {}}>Graphic Design</a>
 								</li>
 							</ul>
 						</div>
@@ -84,19 +84,19 @@ function Footer() {
 							<h4>Our Social Networks</h4>
 							<p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
 							<div className="social-links mt-3">
-								<a href="#" className="twitter">
+								<a href={() => {}} className="twitter">
 									<i className="bx bxl-twitter"></i>
 								</a>
-								<a href="#" className="facebook">
+								<a href={() => {}} className="facebook">
 									<i className="bx bxl-facebook"></i>
 								</a>
-								<a href="#" className="instagram">
+								<a href={() => {}} className="instagram">
 									<i className="bx bxl-instagram"></i>
 								</a>
-								<a href="#" className="google-plus">
+								<a href={() => {}} className="google-plus">
 									<i className="bx bxl-skype"></i>
 								</a>
-								<a href="#" className="linkedin">
+								<a href={() => {}} className="linkedin">
 									<i className="bx bxl-linkedin"></i>
 								</a>
 							</div>
@@ -109,12 +109,12 @@ function Footer() {
 				<div className="copyright">
 					&copy; Copyright{' '}
 					<strong>
-						<span>BizLand</span>
+						<span>Sahiplen</span>
 					</strong>
 					. All Rights Reserved
 				</div>
 				<div className="credits">
-					Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+					kodlayan <a href={() => {}}>Kodluyoruz Proje Ekibi</a>
 				</div>
 			</div>
 		</footer>
