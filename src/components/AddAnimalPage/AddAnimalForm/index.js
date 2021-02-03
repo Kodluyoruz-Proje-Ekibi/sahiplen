a63eabe5a4f39961c6c9493a112b6d21f595dd93
ozanorkun <ozanorkun1@gmail.com>
2021-02-03 22:13:19 +0300

    formik ile form taslağı oluşturuldu

--- src/components/AddAnimalPage/AddAnimalForm/index.js
+++ src/components/AddAnimalPage/AddAnimalForm/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useFormik, TextInput } from 'formik';
+import { useFormik, setFieldValue } from 'formik';
 import validations from './validations';
+import styles from './style.module.css';
 
 function AddAnimalForm() {
 	const formik = useFormik({
@@ -19,51 +20,178 @@ function AddAnimalForm() {
 	});
 	return (
 		<div className="container">
-			<form onSubmit={formik.handleSubmit}>
-				<div>
-					<label>İlan Başlığı</label>
+			<form className={styles.form} onSubmit={formik.handleSubmit}>
+				<div className={styles.formInput}>
+					<label htmlFor="adTitle">İlan Başlığı:</label>
 					<input
+						className="form-control"
 						id="adTitle"
 						name="adTitle"
-						placeholder="İlan Başlığı"
+						placeholder="İlan Başlığı..."
 						value={formik.values.adTitle}
 						onChange={formik.handleChange}
 						onBlur={formik.handleBlur}
 						disabled={formik.isSubmitting}
 					/>
-					{formik.errors.adTitle && formik.touched.adTitle && <div>{formik.errors.adTitle}</div>}
+					{formik.errors.adTitle && formik.touched.adTitle && (
+						<div className={styles.errorMessage}>{formik.errors.adTitle}</div>
+					)}
 				</div>
 
-				<div>
-					<label>İlan Detayı</label>
-					<input
+				<div className={styles.formInput}>
+					<label htmlFor="adDetail">İlan Detayı:</label>
+					<textarea
+						className="form-control"
 						id="adDetail"
 						name="adDetail"
-						placeholder="İlan Detayı"
+						placeholder="İlan Detayı..."
 						value={formik.values.adDetail}
+						rows="4"
+						cols="50"
 						onChange={formik.handleChange}
 						onBlur={formik.handleBlur}
 						disabled={formik.isSubmitting}
 					/>
-					{formik.errors.adDetail && formik.touched.adDetail && <div>{formik.errors.adDetail}</div>}
+					{formik.errors.adDetail && formik.touched.adDetail && (
+						<div className={styles.errorMessage}>{formik.errors.adDetail}</div>
+					)}
 				</div>
 
-				<div>
-					<label>E-mail</label>
+				<div className="row formInput">
+					<div className="col form-group">
+						<label htmlFor="type">Hangi Pet?</label>
+						<input
+							className="form-control"
+							id="type"
+							name="type"
+							placeholder="Örneğin: Kedi"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+
+					<div className="col form-group">
+						<label htmlFor="age">Petin Yaşı:</label>
+						<input
+							className="form-control"
+							id="age"
+							name="age"
+							placeholder="Örneğin: 5"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+				</div>
+
+				<div className="row formInput">
+					<div className="col form-group">
+						<label htmlFor="type">Cinsiyeti:</label>
+						<input
+							className="form-control"
+							id="type"
+							name="type"
+							placeholder="Örneğin: Kedi"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+
+					<div className="col form-group">
+						<label htmlFor="type">Kısırlaştırıldı mı?</label>
+						<input
+							className="form-control"
+							id="type"
+							name="type"
+							placeholder="Örneğin: Kedi"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+				</div>
+
+				<div className="row formInput">
+					<div className="col form-group">
+						<label htmlFor="type">İl:</label>
+						<input
+							className="form-control"
+							id="type"
+							name="type"
+							placeholder="Örneğin: Kedi"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+
+					<div className="col form-group">
+						<label htmlFor="type">İlçe:</label>
+						<input
+							className="form-control"
+							id="type"
+							name="type"
+							placeholder="Örneğin: Kedi"
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							disabled={formik.isSubmitting}
+						/>
+						{formik.errors.email && formik.touched.email && (
+							<div className={styles.errorMessage}>{formik.errors.email}</div>
+						)}
+					</div>
+				</div>
+
+				<div className="formInput">
+					<label htmlFor="email">E-Posta:</label>
 					<input
+						className="form-control"
 						id="email"
 						name="email"
-						placeholder="E-mail"
+						placeholder="E-Posta Adresiniz..."
 						value={formik.values.email}
 						onChange={formik.handleChange}
 						onBlur={formik.handleBlur}
 						disabled={formik.isSubmitting}
 					/>
-					{formik.errors.email && formik.touched.email && <div>{formik.errors.email}</div>}
+					{formik.errors.email && formik.touched.email && (
+						<div className={styles.errorMessage}>{formik.errors.email}</div>
+					)}
 				</div>
 
-				<button type="submit" disabled={formik.isSubmitting}>
-					{formik.isSubmitting ? 'Loading...' : 'Submit'}
+				<div className="formInput">
+					<label htmlFor="image">Fotoğraflar:</label>
+					<input className="form-control" id="image" name="image" type="file" />
+					{formik.errors.image && formik.touched.image && (
+						<div className={styles.errorMessage}>{formik.errors.image}</div>
+					)}
+				</div>
+
+				<button className={styles.adButton} type="submit" disabled={formik.isSubmitting}>
+					{formik.isSubmitting ? 'Gönderiliyor...' : 'Gönder'}
 				</button>
 			</form>
 		</div>
--- src/components/AddAnimalPage/AddAnimalForm/validations.js
+++ src/components/AddAnimalPage/AddAnimalForm/validations.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
-	adTitle: yup.string().required(),
-	adDetail: yup.string().required(),
-	email: yup.string().email().required(),
+	adTitle: yup.string().required('İlan Başlığı Zorunludur!'),
+	adDetail: yup.string().required('İlan Detayı Zorunludur!'),
+	email: yup.string().email().required('E-mail Zorunludur!'),
 });
 
 export default schema;
