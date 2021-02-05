271b3dd52436eff3c5447e7c728d0eace2cf4086
oguuzduran <oguuzduran@outlook.com.tr>
2021-02-05 10:23:01 +0300

    update

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,291 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SiDatadog } from 'react-icons/si';
 import kopek from './kopek.png';
 import './Card.css';
 function Card() {
+	const [pet, setPet] = useState([
+		{ name: 'kedi1' },
+		{ name: 'kedi2' },
+		{ name: 'kedi3' },
+		{ name: 'kedi4' },
+		{ name: 'kedi5' },
+	]);
 	return (
 		<>
 			<div className="container mt-4">
 				<div class="row">
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
+					{pet.map((pet, key) => (
+						<div key={key} className="card-container">
+							<div className="image-container">
+								<img src={kopek} alt="" />
 							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
+							<div className="card-content">
+								<div className="card-title">
+									<h3>
+										{pet.name} <SiDatadog />
+									</h3>
+								</div>
+								<div className="card-body">
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+										incididunt.
+									</p>
+								</div>
 							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
+							<div className="btn" style={{ display: 'flex' }}>
+								<a
+									href="/#"
+									style={{
+										textTransform: 'uppercase',
+										color: 'cadetblue',
+										textDecoration: 'none',
+										fontWeight: 'bold',
+									}}
+								>
+									<h7>Profil</h7>
+								</a>
 							</div>
 						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
-					<div className="card-container">
-						<div className="image-container">
-							<img src={kopek} alt="" />
-						</div>
-						<div className="card-content">
-							<div className="card-title">
-								<h3>
-									Maltipoo <SiDatadog />
-								</h3>
-							</div>
-							<div className="card-body">
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-									incididunt.
-								</p>
-							</div>
-						</div>
-						<div className="btn" style={{ display: 'flex' }}>
-							<a
-								href="/#"
-								style={{
-									textTransform: 'uppercase',
-									color: 'cadetblue',
-									textDecoration: 'none',
-									fontWeight: 'bold',
-								}}
-							>
-								<h7>Profil</h7>
-							</a>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</>
