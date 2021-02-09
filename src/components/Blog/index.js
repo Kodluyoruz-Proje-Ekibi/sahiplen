8db3097ae9fa038032a689905c323be4acc74819
Ozan Orkun <ozanorkun1@gmail.com>
2021-02-09 13:06:27 +0300

    Update index.js
    alt attribute fix

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -83,7 +83,7 @@ function index() {
 			</div>
 			<div className="card-deck">
 					<div className="card">
-					<img className="card-img-top" src={kedi} alt="Card image cap"/>
+					<img className="card-img-top" src={kedi} alt=""/>
 					<div className="card-body">
 					<h5 className="card-title">Card title</h5>
 					<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
