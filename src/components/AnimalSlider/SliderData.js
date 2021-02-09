5126439d003cdfb343f7fbb3dc7791a767745452
busrablt <busrablt2121@gmail.com>
2021-02-09 22:14:02 +0300

    updated

--- src/components/AnimalSlider/SliderData.js
+++ src/components/AnimalSlider/SliderData.js
@@ -1,14 +1,17 @@
+// import image1 from './image1.PNG' yapınca hata aldığım için public e attıp kullandım :(
+
 export const SliderData=[
+
     {
-        image:'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+        image:'./image1.PNG'
     },
 
     {
-        image:'https://images.unsplash.com/photo-1560525821-d5615ef80c69?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+        image:'./image2.PNG'
     },
 
     {
-        image:'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+        image:'./image3.PNG'
     },
 
 ]
