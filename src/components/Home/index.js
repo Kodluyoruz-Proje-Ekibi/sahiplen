eeb0622e004f65675a74933bf869d091615adcd4
engntuba <76241313+engntuba@users.noreply.github.com>
2021-02-09 12:48:35 +0300

    home index.js temizlendi.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Slider from '../Slider';
-<<<<<<< HEAD
-
-
-=======
 import Card from '../Card';
->>>>>>> e1a46adfcc8432970d5e760e053bb1d0c0266ef7
+
 function Home() {
 	return (
 		<>
