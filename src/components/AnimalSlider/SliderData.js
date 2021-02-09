39891de7253def76d67069b651f3eb900cc1b56b
busrablt <busrablt2121@gmail.com>
2021-02-09 21:31:37 +0300

    created animal detail slider

---
+++ src/components/AnimalSlider/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData=[
+    {
+        image:'https://images.unsplash.com/photo-1576201836106-db1758fd1c97?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1560525821-d5615ef80c69?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+    },
+
+    {
+        image:'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
+    },
+
+]
