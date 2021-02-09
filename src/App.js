7da362842915241cf84bf1da2cb9bbadee37382c
busrablt <busrablt2121@gmail.com>
2021-02-09 21:39:16 +0300

    updated animal slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import İmageSlider from './components/AnimalSlider/İmageSlider';
+import { SliderData } from './components/AnimalSlider/SliderData';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<İmageSlider slides={SliderData}/>
 			<Footer />
 		</>
 	);
