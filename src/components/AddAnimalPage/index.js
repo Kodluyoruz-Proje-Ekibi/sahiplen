33ad0cfae1a0f5a359f1fedb1ec3c116404340c0
ozanorkun <ozanorkun1@gmail.com>
2021-02-10 21:31:59 +0300

    animal detail and animalform styling

--- src/components/AddAnimalPage/index.js
+++ src/components/AddAnimalPage/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import AddAnimalForm from './AddAnimalForm';
+import styles from './AddAnimalForm/style.module.css';
 
 function AddAnimalPage() {
 	return (
 		<div>
+			<div className=" container mt-4 ">
+				<div className="row justify-content-center align-items-center">
+					<blockquote className={`blockquote ${styles.blockquote}`}>
+						<p class="mb-0">
+							Küçük dostumuzu sahiplendirmek için öncelikle aşağıdaki formu doldurmalısın...
+						</p>
+					</blockquote>
+				</div>
+			</div>
 			<AddAnimalForm />
 		</div>
 	);
--- src/components/AnimalDetailPage/AnimalCard.js
+++ src/components/AnimalDetailPage/AnimalCard.js
@@ -8,50 +8,50 @@ function AnimalCard() {
 
 			<div className={`container ${styles.animalDescriptionList}`}>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>Tür:</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						Köpek
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>Cinsiyet:</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						Erkek
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>Kısır mı? :</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						Hayır
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>Yaş:</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						3
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>İl:</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						İstanbul
 					</div>
 				</div>
 				<div className="row">
-					<div className="col-6" style={{ textAlign: 'right' }}>
+					<div className="col-6 pr-2" style={{ textAlign: 'right' }}>
 						<b>İlçe:</b>{' '}
 					</div>
-					<div className="col-6" style={{ textAlign: 'left' }}>
+					<div className="col-6 pl-2" style={{ textAlign: 'left' }}>
 						Ümraniye
 					</div>
 				</div>
