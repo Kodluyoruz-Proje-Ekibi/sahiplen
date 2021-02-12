9cd72169fcf87bc92de73c487916c0beec0fef85
oguuzduran <oguuzduran@outlook.com.tr>
2021-02-12 15:10:19 +0300

    blog style update

--- src/components/BlogDetail/index.js
+++ src/components/BlogDetail/index.js
@@ -1,49 +1,76 @@
-import React from 'react'
+import React from 'react';
 import './BlogDetail.css';
 import image1 from './image1.PNG';
 
 function BlogDetail() {
-    return (
-        <div>
-            <div className="container mt-5">
-			<div className="row">
-				
+	return (
+		<div>
+			<div className="container mt-5">
+				<div className="row">
 					<div className="card cardStyle">
 						<img className="img" src={image1} alt="" />
 						<div className="card-body p-3">
-							<h5 className="card-title">Islak Mama Seçerken Nelere Dikkat Edilmelidir? </h5>
+							<h5 className="blog-title">Islak Mama Seçerken Nelere Dikkat Edilmelidir? </h5>
 							<p className="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Quis blandit turpis cursus in hac habitasse. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. Nec tincidunt praesent semper feugiat nibh. Et netus et malesuada fames ac turpis. Ac tortor dignissim convallis aenean et tortor. Habitasse platea dictumst vestibulum rhoncus. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Purus ut faucibus pulvinar elementum. Feugiat scelerisque varius morbi enim nunc faucibus. Ultricies integer quis auctor elit sed vulputate mi sit. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Justo nec ultrices dui sapien eget mi proin. Urna neque viverra justo nec ultrices dui sapien.
-
-                            Vitae et leo duis ut. Rhoncus mattis rhoncus urna neque viverra justo nec. Porttitor massa id neque aliquam vestibulum morbi blandit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Cras sed felis eget velit aliquet sagittis. Mollis nunc sed id semper risus. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Convallis a cras semper auctor neque vitae tempus quam. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Volutpat sed cras ornare arcu.
-
-                            Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. Volutpat consequat mauris nunc congue. Id interdum velit laoreet id donec. Volutpat blandit aliquam etiam erat. In arcu cursus euismod quis viverra. Commodo elit at imperdiet dui. Ornare suspendisse sed nisi lacus sed viverra tellus. Ultrices vitae auctor eu augue ut. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. Proin gravida hendrerit lectus a. Consequat semper viverra nam libero justo. Hendrerit dolor magna eget est lorem ipsum dolor sit.
-
-                            Fermentum posuere urna nec tincidunt. Quis vel eros donec ac. Diam vel quam elementum pulvinar etiam non. Commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Elementum nibh tellus molestie nunc non blandit. Gravida dictum fusce ut placerat orci. Bibendum neque egestas congue quisque. Rhoncus est pellentesque elit ullamcorper dignissim cras. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Morbi tincidunt augue interdum velit. Fermentum dui faucibus in ornare quam viverra. In est ante in nibh mauris cursus mattis molestie. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Vestibulum mattis ullamcorper velit sed. Ac tortor vitae purus faucibus ornare.
-
-                            Nisi est sit amet facilisis magna etiam tempor orci eu. Augue interdum velit euismod in pellentesque massa. Senectus et netus et malesuada fames ac. In eu mi bibendum neque egestas congue quisque egestas. Pharetra diam sit amet nisl. Sed viverra tellus in hac habitasse platea dictumst. Elit pellentesque habitant morbi tristique. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Eget mi proin sed libero enim sed faucibus turpis. Sed vulputate odio ut enim blandit volutpat maecenas. Cum sociis natoque penatibus et magnis. Ultrices sagittis orci a scelerisque purus semper eget duis at. Ut lectus arcu bibendum at varius vel pharetra. Nunc id cursus metus aliquam eleifend. Euismod in pellentesque massa placerat duis ultricies. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer. Libero justo laoreet sit amet cursus sit amet dictum sit. Pretium aenean pharetra magna ac placerat vestibulum lectus. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. In fermentum posuere urna nec tincidunt praesent semper.
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Elit duis tristique sollicitudin nibh
+								sit amet commodo nulla. Quis blandit turpis cursus in hac habitasse. Praesent
+								elementum facilisis leo vel fringilla est ullamcorper eget nulla. Nec tincidunt
+								praesent semper feugiat nibh. Et netus et malesuada fames ac turpis. Ac tortor
+								dignissim convallis aenean et tortor. Habitasse platea dictumst vestibulum rhoncus.
+								Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Purus ut faucibus
+								pulvinar elementum. Feugiat scelerisque varius morbi enim nunc faucibus. Ultricies
+								integer quis auctor elit sed vulputate mi sit. Quam viverra orci sagittis eu
+								volutpat odio facilisis mauris. Justo nec ultrices dui sapien eget mi proin. Urna
+								neque viverra justo nec ultrices dui sapien. Vitae et leo duis ut. Rhoncus mattis
+								rhoncus urna neque viverra justo nec. Porttitor massa id neque aliquam vestibulum
+								morbi blandit. Ipsum dolor sit amet consectetur adipiscing elit pellentesque. Cras
+								sed felis eget velit aliquet sagittis. Mollis nunc sed id semper risus. Egestas quis
+								ipsum suspendisse ultrices gravida dictum fusce. Convallis a cras semper auctor
+								neque vitae tempus quam. Cum sociis natoque penatibus et magnis dis parturient
+								montes nascetur. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean.
+								Volutpat sed cras ornare arcu. Purus faucibus ornare suspendisse sed nisi lacus sed
+								viverra tellus. Volutpat consequat mauris nunc congue. Id interdum velit laoreet id
+								donec. Volutpat blandit aliquam etiam erat. In arcu cursus euismod quis viverra.
+								Commodo elit at imperdiet dui. Ornare suspendisse sed nisi lacus sed viverra tellus.
+								Ultrices vitae auctor eu augue ut. Bibendum ut tristique et egestas quis ipsum
+								suspendisse ultrices gravida. Proin gravida hendrerit lectus a. Consequat semper
+								viverra nam libero justo. Hendrerit dolor magna eget est lorem ipsum dolor sit.
+								Fermentum posuere urna nec tincidunt. Quis vel eros donec ac. Diam vel quam
+								elementum pulvinar etiam non. Commodo quis imperdiet massa tincidunt nunc pulvinar
+								sapien et. Elementum nibh tellus molestie nunc non blandit. Gravida dictum fusce ut
+								placerat orci. Bibendum neque egestas congue quisque. Rhoncus est pellentesque elit
+								ullamcorper dignissim cras. Eget lorem dolor sed viverra ipsum nunc aliquet
+								bibendum. Morbi tincidunt augue interdum velit. Fermentum dui faucibus in ornare
+								quam viverra. In est ante in nibh mauris cursus mattis molestie. Cursus risus at
+								ultrices mi tempus imperdiet nulla malesuada. Vestibulum mattis ullamcorper velit
+								sed. Ac tortor vitae purus faucibus ornare. Nisi est sit amet facilisis magna etiam
+								tempor orci eu. Augue interdum velit euismod in pellentesque massa. Senectus et
+								netus et malesuada fames ac. In eu mi bibendum neque egestas congue quisque egestas.
+								Pharetra diam sit amet nisl. Sed viverra tellus in hac habitasse platea dictumst.
+								Elit pellentesque habitant morbi tristique. Elementum nisi quis eleifend quam
+								adipiscing vitae proin sagittis nisl. Eget mi proin sed libero enim sed faucibus
+								turpis. Sed vulputate odio ut enim blandit volutpat maecenas. Cum sociis natoque
+								penatibus et magnis. Ultrices sagittis orci a scelerisque purus semper eget duis at.
+								Ut lectus arcu bibendum at varius vel pharetra. Nunc id cursus metus aliquam
+								eleifend. Euismod in pellentesque massa placerat duis ultricies. Aliquet sagittis id
+								consectetur purus ut faucibus pulvinar elementum integer. Libero justo laoreet sit
+								amet cursus sit amet dictum sit. Pretium aenean pharetra magna ac placerat
+								vestibulum lectus. Ullamcorper morbi tincidunt ornare massa eget egestas purus
+								viverra. In fermentum posuere urna nec tincidunt praesent semper.
 							</p>
-                            <p className="card-text">
+							<p className="card-text">
 								<small class="text-muted">Blog Yazarı:X</small>
-                                
 							</p>
 							<p className="card-text ">
 								<small class="text-muted">14.02.21</small>
-                                
 							</p>
-                           
-							
 						</div>
-					</div>
-
-					
 					</div>
 				</div>
 			</div>
-		
-            
-        
-    )
+		</div>
+	);
 }
 
-export default BlogDetail
+export default BlogDetail;
